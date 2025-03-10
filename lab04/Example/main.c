963131350020bce90caad6f26514da273a85cafa
TomieRR <136272336+TomieRR@users.noreply.github.com>
2025-03-10 11:39:15 +0100

    lab04

--- lab04/Example/main.c
+++ lab04/Example/main.c
@@ -10,3 +10,22 @@ int main()
     printf("%d\n",17 >> 2);
     return 0;
 }
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    printf("Podaj pierwsza liczbe\n";
+    scanf("%d",a);
+    printf("Podaj druga liczbe\n";
+    scanf("%d",b);
+    printf("%d\n",a & b);
+    printf("%d\n",a | b);
+    return 0;
+}
